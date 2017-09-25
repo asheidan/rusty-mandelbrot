b1827df73d695ca7f1d8c9e8049b150145a8418a
Emil Eriksson <emil.eriksson@codemill.se>
2017-09-25 08:53:34 +0200

    Tweak the way the area to render is specified

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,15 @@ use ppm::PPM;
 use palette::palette;
 use render::iterate;
 
+const CEN_REAL: f64 = -0.7345;
+const CEN_IMAG: f64 = 0.1955;
+const INT_REAL: f64 = 0.0015;
+const INT_IMAG: f64 = 0.001;
 
-const MIN_X: f64 = -2.0;
-const MIN_Y: f64 = -1.0;
-const MAX_X: f64 = 1.0;
-const MAX_Y: f64 = 1.0;
+const MIN_X: f64 = CEN_REAL - (INT_REAL / 2.0);  // -2.0;
+const MIN_Y: f64 = CEN_IMAG - (INT_IMAG / 2.0);  // -1.0;
+const MAX_X: f64 = CEN_REAL + (INT_REAL / 2.0);  // 1.0;
+const MAX_Y: f64 = CEN_IMAG + (INT_IMAG / 2.0);  // 1.0;
 
 const RES_X: u32 = 2560 * 2;
 const RES_Y: u32 = 1440 * 2;
@@ -29,9 +33,11 @@ fn main() {
 
     println!("Rendering image");
     for y in 0..RES_Y {
-        let imaginary_coord = MIN_Y + (y as f64) * step_y;
+        // Going top to bottom
+        let imaginary_coord = MAX_Y - (y as f64) * step_y;
 
         for x in 0..RES_X {
+            // Going left to right
             let real_coord = MIN_X + (x as f64) * step_x;
 
             let color = palette(iterate(real_coord, imaginary_coord));
